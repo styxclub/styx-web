8355626e7b022072c34a263ad5005bb1cb3e851f
Iñigo Gorosabel <inigo.gorosabel@idom.com>
2025-09-25 11:59:32 +0200

    Adapto popup parameter para moviles

--- src/app/shared/popup-parameter/popup-parameter.ts
+++ src/app/shared/popup-parameter/popup-parameter.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core';
 import AuthStore from '@auth/auth-store';
 import Parameter from '@model/parameter.model';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-popup-parameter',
@@ -18,12 +19,29 @@ import Parameter from '@model/parameter.model';
 })
 export default class PopupParameter implements OnInit {
   authStore: AuthStore = inject(AuthStore);
-  id: InputSignal<number> = input.required<number>();
+
+  public config: DynamicDialogConfig = inject(DynamicDialogConfig);
+  id: InputSignal<number | null> = input<number | null>(null);
   title: WritableSignal<string> = signal<string>('');
   body: WritableSignal<string> = signal<string>('');
+  isMobile: WritableSignal<boolean> = signal<boolean>(false);
 
   ngOnInit(): void {
-    const param: Parameter | null = this.authStore.getParameterById(this.id());
+    const id: number | null = this.id();
+    if (id !== null) {
+      this.loadParameter(id);
+    }
+    if (this.config && this.config.data) {
+      this.isMobile.set(true);
+      const idParam: number | undefined = this.config.data['id'];
+      if (idParam) {
+        this.loadParameter(idParam);
+      }
+    }
+  }
+
+  loadParameter(id: number): void {
+    const param: Parameter | null = this.authStore.getParameterById(id);
     if (param) {
       this.title.set(param.title ?? '');
       this.body.set(param.body ?? '');
--- src/app/shared/request-item/request-item.ts
+++ src/app/shared/request-item/request-item.ts
@@ -1,9 +1,12 @@
-import { Component, input, InputSignal, OnInit } from '@angular/core';
+import { Component, inject, input, InputSignal, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RequestParameter } from '@interfaces/home.interfaces';
 import Request from '@model/request.model';
+import PopupParameter from '@shared/popup-parameter/popup-parameter';
 import PopupParameterDirective from '@shared/popup-parameter/popup-parameter-directive';
 import UserPhoto from '@shared/user-photo/user-photo';
 import { ButtonModule } from 'primeng/button';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
@@ -13,11 +16,24 @@ import { TooltipModule } from 'primeng/tooltip';
   styleUrl: './request-item.scss',
 })
 export default class RequestItem implements OnInit {
+  dialogService: DialogService = inject(DialogService);
+
   request: InputSignal<Request> = input.required<Request>();
+  isMobile: InputSignal<boolean> = input.required<boolean>();
   idUser: number | null | undefined = null;
+  ref: DynamicDialogRef | undefined;
 
   ngOnInit(): void {
-    console.log(this.request());
     this.idUser = this.request().user?.id;
+  }
+
+  showParameter(parameter: RequestParameter): void {
+    this.ref = this.dialogService.open(PopupParameter, {
+      header: parameter.title,
+      width: '75vw',
+      modal: true,
+      closable: true,
+      data: { id: parameter.idParameter },
+    });
   }
 }
